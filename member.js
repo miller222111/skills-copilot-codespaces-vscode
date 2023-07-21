10494456440f77b6bac10022e8aed292be031b42
Andrew <miller222111@users.noreply.github.com>
2023-07-21 18:08:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "PHP"];
+    var member = {
+        name: "John",
+        age: 25,
+        skills: skills
+    };
+    return member.skills[2];
+}
